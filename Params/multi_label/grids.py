4912f5c6a517b75ba1b2bb579fbc71a1b4879170
Rosa Lavelle-Hill <51444424+Rosa-Lavelle-Hill@users.noreply.github.com>
2025-04-11 08:32:43 +0200

    Update comments on final grids

--- Params/multi_label/grids.py
+++ Params/multi_label/grids.py
@@ -18,47 +18,47 @@ enet_params = {"ml__estimator__C":[0.001, 0.01, 0.5, 1], # Inverse of regulariza
               "ml__estimator__max_iter": [1000]}
 
 # smote ml grids -------------------------------------------------------------
-# rf_params_sm = {"estimator__classifier__min_samples_split": [2, 4, 6],
-#              "estimator__classifier__max_depth": [5, 10],
-#              "estimator__classifier__n_estimators": [500, 800],
-#              "estimator__classifier__random_state": [93],
-#              "estimator__classifier__max_features": [0.7, None],
-#              "estimator__oversampler__k_neighbors": [3, 5],
-#              "estimator__oversampler__sampling_strategy": ['auto']
-# }
-# ^ grid 24th Jan
+# Final Grid RF: 
+rf_params_sm = {"estimator__classifier__min_samples_split": [2, 4, 6],
+             "estimator__classifier__max_depth": [5, 10],
+             "estimator__classifier__n_estimators": [500, 800],
+             "estimator__classifier__random_state": [93],
+             "estimator__classifier__max_features": [0.7, None],
+             "estimator__oversampler__k_neighbors": [3, 5],
+             "estimator__oversampler__sampling_strategy": ['auto']
+}
 
-# if run again:
-rf_params_sm ={'estimator__classifier__max_depth': [10, 12, 15, 17],
-               'estimator__classifier__max_features': [0.7, 0.85, 0.9, 1],
-               'estimator__classifier__min_samples_split': [2, 3, 4],
-               'estimator__classifier__n_estimators': [800],
-               'estimator__classifier__random_state': [93],
-               'estimator__oversampler__k_neighbors': [3, 5, 7],
-               'estimator__oversampler__sampling_strategy': ['auto']}
+# Additionally ran this larger grid, but no increase in performance so kept the original grid
+# rf_params_sm ={'estimator__classifier__max_depth': [10, 12, 15, 17],
+#                'estimator__classifier__max_features': [0.7, 0.85, 0.9, 1],
+#                'estimator__classifier__min_samples_split': [2, 3, 4],
+#                'estimator__classifier__n_estimators': [800],
+#                'estimator__classifier__random_state': [93],
+#                'estimator__oversampler__k_neighbors': [3, 5, 7],
+#                'estimator__oversampler__sampling_strategy': ['auto']}
 
-# gb_params_sm = {"estimator__classifier__min_samples_leaf": [2, 4, 6],
-#                 "estimator__classifier__max_depth": [5, 10],
-#                 "estimator__classifier__random_state": [93],
-#                 "estimator__classifier__n_estimators": [500, 800],
-#                 "estimator__classifier__max_features": [0.7, None],
-#                 "estimator__classifier__learning_rate": [0.01, 0.05, 0.1],
-#                 "estimator__oversampler__k_neighbors": [3, 5],
-#                 "estimator__oversampler__sampling_strategy": ['auto'],
-#                 "estimator__classifier__subsample": [0.8, 1.0],
-#                 }
-# ^ grid 24th Jan
+# Final Grid GB:
+gb_params_sm = {"estimator__classifier__min_samples_leaf": [2, 4, 6],
+                "estimator__classifier__max_depth": [5, 10],
+                "estimator__classifier__random_state": [93],
+                "estimator__classifier__n_estimators": [500, 800],
+                "estimator__classifier__max_features": [0.7, None],
+                "estimator__classifier__learning_rate": [0.01, 0.05, 0.1],
+                "estimator__oversampler__k_neighbors": [3, 5],
+                "estimator__oversampler__sampling_strategy": ['auto'],
+                "estimator__classifier__subsample": [0.8, 1.0],
+                }
 
-# if run again:
-gb_params_sm = {'estimator__classifier__learning_rate': [0.001, 0.01, 0.05],
-                'estimator__classifier__max_depth': [3, 5, 10],
-                'estimator__classifier__max_features': [9, 11, 1.0], # /11, before 0.7 ==8
-                'estimator__classifier__min_samples_leaf': [2, 3, 4],
-                'estimator__classifier__n_estimators': [800],
-                'estimator__classifier__random_state': [93],
-                'estimator__classifier__subsample': [0.7, 0.8],
-                'estimator__oversampler__k_neighbors': [3, 5, 7],
-                'estimator__oversampler__sampling_strategy': ['auto']}
+# Additionally ran this larger grid, but no increase in performance so kept the original grid
+# gb_params_sm = {'estimator__classifier__learning_rate': [0.001, 0.01, 0.05],
+#                 'estimator__classifier__max_depth': [3, 5, 10],
+#                 'estimator__classifier__max_features': [9, 11, 1.0], # /11, before 0.7 ==8
+#                 'estimator__classifier__min_samples_leaf': [2, 3, 4],
+#                 'estimator__classifier__n_estimators': [800],
+#                 'estimator__classifier__random_state': [93],
+#                 'estimator__classifier__subsample': [0.7, 0.8],
+#                 'estimator__oversampler__k_neighbors': [3, 5, 7],
+#                 'estimator__oversampler__sampling_strategy': ['auto']}
 
 log_params_sm = {"estimator__classifier__penalty": [None]}
 
